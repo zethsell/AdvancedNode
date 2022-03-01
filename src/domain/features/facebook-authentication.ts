ff4f914de244a9170ea16776590f32af294b49e2
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-03-01 09:32:11 -0300

    add facebook authentication feature

---
+++ src/domain/features/facebook-authentication.ts
@@ -0,0 +1,13 @@
+import { AuthenticationError } from '@/domain/errors/authentication'
+import { AccessToken } from '@/domain/models/access-token'
+export interface FacebookAuthentication {
+  perform: (params: FacebookAuthentication.Params) => Promise<FacebookAuthentication.Result>
+}
+
+namespace FacebookAuthentication {
+  export type Params = {
+    token: string
+  }
+
+  export type Result = AccessToken | AuthenticationError
+}
