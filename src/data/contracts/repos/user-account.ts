6c5b558f61a349f4ce7b7468ad308bf5db6439c6
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-03-04 10:16:33 -0300

    ensure service call TokenGenerator with correct params

--- src/data/contracts/repos/user-account.ts
+++ src/data/contracts/repos/user-account.ts
@@ -12,7 +12,7 @@ export namespace LoadUserAccountRepository {
 }
 
 export interface SaveFacebookAccountRepository {
-  saveWithFacebook: (params: SaveFacebookAccountRepository.Params) => Promise<void>
+  saveWithFacebook: (params: SaveFacebookAccountRepository.Params) => Promise<SaveFacebookAccountRepository.Result>
 }
 
 export namespace SaveFacebookAccountRepository {
@@ -21,5 +21,9 @@ export namespace SaveFacebookAccountRepository {
     name: string
     email: string
     facebookId: string
+  }
+
+  export type Result = {
+    id: string
   }
 }
