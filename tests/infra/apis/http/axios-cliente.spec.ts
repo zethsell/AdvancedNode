5759f185f1dd9c9394d6bf382cfe76e7971099a6
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-03-07 20:54:20 -0300

    ensure AxiosHttpClient returns data on success

--- tests/infra/apis/http/axios-cliente.spec.ts
+++ tests/infra/apis/http/axios-cliente.spec.ts
@@ -4,8 +4,9 @@ import axios from 'axios'
 jest.mock('axios')
 
 class AxiosHttpClient {
-  async get (args: HttpGetClient.Params): Promise<void> {
-    await axios.get(args.url, { params: args.params })
+  async get (args: HttpGetClient.Params): Promise<any> {
+    const result = await axios.get(args.url, { params: args.params })
+    return result.data
   }
 }
 
@@ -17,6 +18,10 @@ describe('AxiosHttpClient', () => {
 
   beforeAll(() => {
     fakeAxios = axios as jest.Mocked<typeof axios>
+    fakeAxios.get.mockResolvedValue({
+      status: 200,
+      data: 'any_data'
+    })
     url = 'any_url'
     params = { any: 'any' }
   })
@@ -30,8 +35,13 @@ describe('AxiosHttpClient', () => {
       await sut.get({ url, params })
 
       expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
-
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
+    })
+
+    it('should return data on success', async () => {
+      const result = await sut.get({ url, params })
+
+      expect(result).toEqual('any_data')
     })
   })
 })
