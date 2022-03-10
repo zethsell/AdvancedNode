3b7f77131e5df6917e9152086437c80d0b0d52bd
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-03-09 21:49:17 -0300

    move production code to their own files

---
+++ src/infra/postgres/repos/user-account.ts
@@ -0,0 +1,17 @@
+import { PgUser } from '@/infra/postgres/entities/user'
+import { LoadUserAccountRepository } from '@/data/contracts/repos'
+
+import { getRepository } from 'typeorm'
+
+export class PgUserAccountRepository implements LoadUserAccountRepository {
+  async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
+    const pgUserRepo = getRepository(PgUser)
+    const pgUser = await pgUserRepo.findOne({ email: params.email })
+    if (pgUser !== undefined) {
+      return {
+        id: pgUser.id.toString(),
+        name: pgUser.name ?? undefined
+      }
+    }
+  }
+}
