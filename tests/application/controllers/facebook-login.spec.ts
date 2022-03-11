ad4f4acdbadaf36fa507eac6c591aff01ef46d36
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-03-11 20:49:10 -0300

    ensure FacebookLoginController return 400 if token is null

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -34,15 +34,4 @@ describe('FacebokLoginController', () => {
       data: new Error('The field token is required')
     })
   })
-
-  it('should return 400 if token is undefined', async () => {
-    const sut = new FacebookLoginController()
-
-    const httpResponse = await sut.handle({ token: '' })
-
-    expect(httpResponse).toEqual({
-      statusCode: 400,
-      data: new Error('The field token is required')
-    })
-  })
 })
