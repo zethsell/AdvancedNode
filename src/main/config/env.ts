4187aa3cde96dc47ff3d802619e117a4aaa29caf
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-03-14 13:17:33 -0300

    configure express server

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? '504382084594930',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '658532334f376aaa33a25aff4b529387'
-  }
+  },
+  port: process.env.PORT ?? 4444
 }
