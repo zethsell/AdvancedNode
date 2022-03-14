350f4aee50fa6658f01015015883634f847457e8
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-03-13 22:55:36 -0300

    apply composite

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,4 +1,4 @@
-import { RequiredStringValidator } from '@/application/validation/required-string'
+import { RequiredStringValidator, ValidationComposite } from '@/application/validation'
 import { HttpResponse, badRequest, unauthorized, serverError, ok } from '@/application/helpers'
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
@@ -33,7 +33,8 @@ export class FacebookLoginController {
   }
 
   private validate (httpRequest: HttpRequest): Error | undefined {
-    const validator = new RequiredStringValidator(httpRequest.token, 'token')
-    return validator.validate()
+    return new ValidationComposite([
+      new RequiredStringValidator(httpRequest.token, 'token')
+    ]).validate()
   }
 }
