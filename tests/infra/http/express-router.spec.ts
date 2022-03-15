9ffb9084dcf8ade9ad167de2afaa47be57e49a47
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-03-15 17:29:51 -0300

    isolate sut creation

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import { getMockReq, getMockRes } from '@jest-mock/express'
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 import { Controller } from '@/application/controllers'
 
 class ExpressRouter {
@@ -11,12 +11,18 @@ class ExpressRouter {
   }
 }
 describe('ExpressRouter', () => {
-  it('should call handle with correct request', async () => {
-    const req = getMockReq({ body: { any: 'any' } })
-    const { res } = getMockRes()
-    const controller = mock<Controller>()
-    const sut = new ExpressRouter(controller)
+  let req: Request
+  let res: Response
+  let controller: MockProxy<Controller>
+  let sut: ExpressRouter
 
+  beforeEach(() => {
+    req = getMockReq({ body: { any: 'any' } })
+    res = getMockRes().res
+    controller = mock()
+    sut = new ExpressRouter(controller)
+  })
+  it('should call handle with correct request', async () => {
     await sut.adapt(req, res)
 
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
@@ -24,12 +30,9 @@ describe('ExpressRouter', () => {
 
   it('should call handle with empty request', async () => {
     const req = getMockReq()
-    const { res } = getMockRes()
-    const controller = mock<Controller>()
-    const sut = new ExpressRouter(controller)
 
     await sut.adapt(req, res)
 
-    expect(controller.handle).toHaveBeenCalledWith({ })
+    expect(controller.handle).toHaveBeenCalledWith({})
   })
 })
