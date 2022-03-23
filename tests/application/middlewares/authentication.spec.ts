893ef9504f89d21541d931707a849eaf2c17dd1a
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-03-22 21:52:26 -0300

    ensure return 403 if authorize throws

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,19 +1,35 @@
 import { ForbiddenError } from '@/application/errors'
 import { forbidden, HttpResponse } from '@/application/helpers'
+import { RequiredStringValidator } from '@/application/validation'
+import { Authorize } from '@/domain/usecases'
 
 type HttpRequest = { authorization: string }
 
 class AuthenticationMiddleware {
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse<Error>> {
-    return forbidden()
+  constructor (private readonly authorize: Authorize) {}
+  async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Error> | undefined> {
+    const error = new RequiredStringValidator(authorization, 'authorization').validate()
+    if (error !== undefined) return forbidden()
+    try {
+      await this.authorize({ token: authorization })
+    } catch {
+      return forbidden()
+    }
   }
 }
 
 describe('AuthenticationMiddleware', () => {
   let sut: AuthenticationMiddleware
+  let authorize: jest.Mock
+  let authorization: string
+
+  beforeAll(() => {
+    authorization = 'any_authorization_token'
+    authorize = jest.fn()
+  })
 
   beforeEach(() => {
-    sut = new AuthenticationMiddleware()
+    sut = new AuthenticationMiddleware(authorize)
   })
 
   it('should return 403 if authorization is empty', async () => {
@@ -36,6 +52,24 @@ describe('AuthenticationMiddleware', () => {
 
   it('should return 403 if authorization is undefined', async () => {
     const httpResponse = await sut.handle({ authorization: undefined as any })
+
+    expect(httpResponse).toEqual({
+      statusCode: 403,
+      data: new ForbiddenError()
+    })
+  })
+
+  it('should call authorize with correct input', async () => {
+    await sut.handle({ authorization })
+
+    expect(authorize).toHaveBeenCalledWith({ token: authorization })
+    expect(authorize).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 403 if authorize throws', async () => {
+    authorize.mockRejectedValueOnce(new Error('any_error'))
+
+    const httpResponse = await sut.handle({ authorization })
 
     expect(httpResponse).toEqual({
       statusCode: 403,
