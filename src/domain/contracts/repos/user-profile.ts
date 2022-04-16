2325f25de4c1c185b57f73a143382779e4b43e69
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-04-16 17:49:46 -0300

    feat: ensure ChangeProfilePicture calls LoadUserProfile with correct input

--- src/domain/contracts/repos/user-profile.ts
+++ src/domain/contracts/repos/user-profile.ts
@@ -6,3 +6,11 @@ export namespace SaveUserPicture {
   export type Input = { pictureUrl?: string }
 
 }
+
+export interface LoadUserProfile {
+  load: (input: LoadUserProfile.Input) => Promise<void>
+}
+
+export namespace LoadUserProfile {
+  export type Input = { id: string }
+}
