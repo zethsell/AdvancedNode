f6abdbe4be2bca00d42f2d10f94acd1313be6977
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-04-23 15:02:01 -0300

    --amend

--- tests/application/controllers/save-profile-picture.spec.ts
+++ tests/application/controllers/save-profile-picture.spec.ts
@@ -16,8 +16,17 @@ describe('SaveProfileController', () => {
   beforeEach(() => {
     sut = new SavePictureController()
   })
+
   it('should return 400 if file is not provided', async () => {
     const httpResponse = await sut.handle({ file: undefined })
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
+
+  it('should return 400 if file is not provided', async () => {
+    const httpResponse = await sut.handle({ file: null })
     expect(httpResponse).toEqual({
       statusCode: 400,
       data: new RequiredFieldError('file')
