48f331ac3a7911b33a505efa5212f019ed665e43
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-04-23 17:29:14 -0300

    ensure SaePictureController returns 400 if file size is more than 5MB

--- tests/application/controllers/save-profile-picture.spec.ts
+++ tests/application/controllers/save-profile-picture.spec.ts
@@ -9,6 +9,7 @@ class SavePictureController {
     if (file === undefined || file === null) return badRequest(new RequiredFieldError('file'))
     if (file.buffer.length === 0) return badRequest(new RequiredFieldError('file'))
     if (!['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimeType)) return badRequest(new InvalidMymeTypeError(['jpeg', 'png']))
+    if (file.buffer.length > 5 * 1024 * 1024) return badRequest(new MaxFileSizeError(5))
   }
 }
 
@@ -16,6 +17,12 @@ class InvalidMymeTypeError extends Error {
   constructor (allowed: string[]) {
     super(`Unsupported type. Allowed types: ${allowed.join(', ')}`)
     this.name = 'InvalidMymeTypeError'
+  }
+}
+class MaxFileSizeError extends Error {
+  constructor (maxSizeInMb: number) {
+    super(`File upload limit is ${maxSizeInMb}MB`)
+    this.name = 'MaxFileSizeError'
   }
 }
 
@@ -86,6 +93,15 @@ describe('SaveProfileController', () => {
     expect(httpResponse).not.toEqual({
       statusCode: 400,
       data: new InvalidMymeTypeError(['jpeg', 'png'])
+    })
+  })
+
+  it('should return 400 if file size is bigger than 5MB', async () => {
+    const invalidBuffer = Buffer.from(new ArrayBuffer(6 * 1024 * 1024))
+    const httpResponse = await sut.handle({ file: { buffer: invalidBuffer, mimeType } })
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new MaxFileSizeError(5)
     })
   })
 })
