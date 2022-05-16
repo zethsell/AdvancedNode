1a7063691fb1e74d2dc0a3daa04a0ccab296ca97
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2022-05-16 13:42:50 -0300

    ensure PUT /users/picture returns 200 on success

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,9 +1,9 @@
 import { auth } from '@/main/middlewares/authentication'
 import { Router } from 'express'
 import { makeSavePictureController } from '@/main/factories/controllers'
-import { adaptExpressRoute as adapt } from '@/main/adapters'
+import { adaptExpressRoute as adapt, adaptMulter as upload } from '@/main/adapters'
 
 export default (router: Router): void => {
   router.delete('/users/picture', auth, adapt(makeSavePictureController()))
-  router.put('/users/picture', auth)
+  router.put('/users/picture', auth, upload, adapt(makeSavePictureController()))
 }
